70f695fc19589457832e9b98e101e20d275d1fa2
Travis Gandy <travis_gandy@yahoo.com>
2020-08-20 07:00:51 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
+=======
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+>>>>>>> e1fbff4192918f286a00fd0427335e494013ca2e
   def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
+=======
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+>>>>>>> e1fbff4192918f286a00fd0427335e494013ca2e
   def change
     change_column :students, :birthdate, :datetime
   end
